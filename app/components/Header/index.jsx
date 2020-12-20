6dc0a269ee7071753da8639f1095230502c07f70
zhanghuan <2387749855@qq.com>
2020-12-20 20:55:18 +0800

    react性能优化方案demo

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
+import PureRenderMixin from "react-addons-pure-render-mixin";
 
 export default class Header extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
 
     render() {
         return (
