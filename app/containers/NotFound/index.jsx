8f379bddcc4d4bfca9dfa3179d843f28a75726da
zhanghuan <2387749855@qq.com>
2020-12-20 21:55:58 +0800

    完善react router代码

---
+++ app/containers/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <p>404 NotFound</p>
+        )
+    }
+}
--- app/index.jsx
+++ app/index.jsx
@@ -3,16 +3,10 @@ import { render } from 'react-dom'
 
 // 通用样式
 import './static/css/common.less'
-
-import Todo from './containers/Todo';
-
-// 性能测试
-import Perf from 'react-addons-perf'
-if (__DEV__) {
-    window.Perf = Perf
-}
+import {hashHistory} from 'react-router';
+import RouteMap from './router/routeMap';
 
 render(
-    <Todo/>,
+    <RouteMap hashHistory={hashHistory}/>,
     document.getElementById('root')
 )
